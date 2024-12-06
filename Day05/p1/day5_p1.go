1d7d5334f2d7e8d0f11bcc2c0bdd71e967297161
Cdawn99 <140974895+Cdawn99@users.noreply.github.com>
2024-12-07 00:06:59 +0100

    Day 5

---
+++ Day05/p1/day5_p1.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "os"
+    "slices"
+    "strconv"
+    "strings"
+)
+
+func main() {
+    program := os.Args[0]
+    fp := os.Args[1:]
+
+    if (len(fp) != 1) {
+        fmt.Printf("Usage: %v <page_info.txt>\n", program)
+        os.Exit(1)
+    }
+
+    page_info_bytes, err := os.ReadFile(fp[0])
+    if err != nil {
+        log.Fatalf("Failed to open file %v\n", fp[0])
+    }
+    page_info_str := string(page_info_bytes)
+    page_info_slice := strings.Split(strings.TrimSpace(page_info_str), "\n\n")
+
+    rules := strings.Split(strings.TrimSpace(page_info_slice[0]), "\n")
+
+    rev_adj_map := make(map[string][]string)
+    for _, v := range rules {
+        vs := strings.Split(v, "|")
+        pre := vs[0]
+        post := vs[1]
+        rev_adj_map[post] = append(rev_adj_map[post], pre)
+    }
+
+    updates := strings.Split(strings.TrimSpace(page_info_slice[1]), "\n")
+    middle_sum := 0
+    for _, update := range updates {
+        pages := strings.Split(update, ",")
+        pages_count := len(pages)
+
+        is_correct_order := true
+        for i := 0; i < pages_count; i++ {
+            prev_pages := rev_adj_map[pages[i]]
+            for j := i + 1; j < pages_count; j++ {
+                if slices.Contains(prev_pages, pages[j]) {
+                    is_correct_order = false
+                    break
+                }
+            }
+            if !is_correct_order {
+                break
+            }
+        }
+        if !is_correct_order {
+            continue
+        }
+
+        mid_idx := pages_count / 2
+        mid_val, err := strconv.Atoi(pages[mid_idx])
+        if err != nil {
+            log.Fatalf("Filed to convert %v to int\n", pages[mid_idx])
+        }
+        middle_sum += mid_val
+    }
+
+    fmt.Printf("Sum of middle pages: %v\n", middle_sum)
+}
