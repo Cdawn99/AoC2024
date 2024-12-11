77d80b6ced0c6215a6d4c036535df635fd334f9a
Cdawn99 <140974895+Cdawn99@users.noreply.github.com>
2024-12-11 22:42:22 +0100

    Day 9 part 1

--- Day09/day9_p1.f90
+++ Day09/day9_p1.f90
@@ -4,8 +4,16 @@ program day9
 
     character(32) :: program_name
     character(32) :: filepath
-    character(:), allocatable :: disk_map
     integer :: rstat
+
+    character(:), allocatable :: disk_map_str
+    integer :: i, n
+    integer (kind=8) :: sum
+
+    integer, allocatable :: disk_map(:)
+    integer :: j, idx, id
+    integer, allocatable :: decompressed_disk_map(:)
+    integer :: temp
 
     call get_command_argument(0, program_name)
     if (command_argument_count() /= 1) then
@@ -14,12 +22,75 @@ program day9
     end if
 
     call get_command_argument(1, filepath)
-    disk_map = read_entire_file(filepath, rstat)
+    disk_map_str = read_entire_file(filepath, rstat)
     if (rstat /= 0) then
         stop
     end if
 
-    print '(A)', disk_map
+    allocate(disk_map(len(disk_map_str)))
+    sum = 0
+    do i = 1, len(disk_map_str)
+        n = iachar(disk_map_str(i:i)) - iachar('0')
+        disk_map(i) = n
+        sum = sum + n
+    end do
+    deallocate(disk_map_str)
 
+    allocate(decompressed_disk_map(sum))
+
+    idx = 1
+    id = 0
+    do i = 1, size(disk_map)
+        do j = 1, disk_map(i)
+            if (mod(i, 2) /= 0) then
+                decompressed_disk_map(idx) = id
+            else
+                decompressed_disk_map(idx) = -1
+            end if
+            idx = idx + 1
+        end do
+        if (mod(i, 2) /= 0) then
+            id = id + 1
+        end if
+    end do
     deallocate(disk_map)
+
+    i = 1
+    j = size(decompressed_disk_map)
+    do
+        do
+            if (decompressed_disk_map(i) /= -1) then
+                i = i + 1
+            else
+                exit
+            end if
+        end do
+        do
+            if (decompressed_disk_map(j) == -1) then
+                j = j - 1
+            else
+                exit
+            end if
+        end do
+        if (i > j) then
+            exit
+        end if
+        temp = decompressed_disk_map(i)
+        decompressed_disk_map(i) = decompressed_disk_map(j)
+        decompressed_disk_map(j) = temp
+    end do
+
+    sum = 0
+    i = 0
+    do
+        sum = sum + i * decompressed_disk_map(i + 1)
+        i = i + 1
+        if (decompressed_disk_map(i + 1) == -1) then
+            exit
+        end if
+    end do
+
+    print *, sum
+
+    deallocate(decompressed_disk_map)
 end program day9
